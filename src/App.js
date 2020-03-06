c760067405c0d00b1bd01df43cedd95799381eb1
Ili Pascu <xipascu@gmail.com>
2020-03-06 12:36:31 +0100

    added components, navbar comp working, and css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import BookContextProvider from './contexts/BookContext';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <BookContextProvider>
+        <Navbar />
+      </BookContextProvider>
     </div>
   );
 }
--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,10 +1,27 @@
 import React, { createContext, useState } from 'react';
-
+import { v1 as uuid } from 'uuid';
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-  useState([
-    {title: 'The Lean Startup', id:'1'}
-    {title: 'Atomic Habits', id:'2'}
+  const [books, setBooks] = useState([
+    {title: 'The Lean Startup', author: 'duno', id:'1'},
+    {title: 'Atomic Habits', author: 'duno', id:'2'}
   ])
+
+  const addBook = (title, author) => {
+    setBooks([...books, { title, author, id: uuid() }])
+  }
+
+  const removeBook = (id) => {
+    setBooks(books.filter(book => book.id !== id));
+  }
+
+  return (
+    <BookContext.Provider value={{ books, addBook, removeBook }}>
+      {props.children}
+    </BookContext.Provider>
+  )
 }
+
+export default BookContextProvider
+
