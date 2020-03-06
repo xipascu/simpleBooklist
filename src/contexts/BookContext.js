00a68c9847f59b2fa3d8796f32e4b86063d55b4f
Ili Pascu <xipascu@gmail.com>
2020-03-06 14:05:16 +0100

    added reducers instead of using useContext

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,23 +1,12 @@
-import React, { createContext, useState } from 'react';
-import { v1 as uuid } from 'uuid';
+import React, { createContext, useReducer } from 'react';
+import { bookReducer } from '../reducers/bookReducer';
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-  const [books, setBooks] = useState([
-    {title: 'The Lean Startup', author: 'duno', id:'1'},
-    {title: 'Atomic Habits', author: 'duno', id:'2'}
-  ])
-
-  const addBook = (title, author) => {
-    setBooks([...books, { title, author, id: uuid() }])
-  }
-
-  const removeBook = (id) => {
-    setBooks(books.filter(book => book.id !== id));
-  }
+  const [books, dispatch] = useReducer( bookReducer, []);
 
   return (
-    <BookContext.Provider value={{ books, addBook, removeBook }}>
+    <BookContext.Provider value={{ books, dispatch }}>
       {props.children}
     </BookContext.Provider>
   )
